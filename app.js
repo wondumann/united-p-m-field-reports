093659cd1068de5d06bbbe535e3f88bf82e3cdec
wondumann <wonduman@gmail.com>
2026-06-04 22:51:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -362,11 +362,9 @@ async function saveToGoogleSheets(state) {
     );
 
     const response = await fetch(
-    await fetch(
       "https://script.google.com/macros/s/AKfycbwcdIQvGAV42sCenjPdq1Gk3RNDiLxQAzY-YI6erEIMoSFQ3aupnF8kYS-kbf2Kqv20TQ/exec",
       {
         method: "POST",
-        mode: "no-cors",
         body: formData
       }
     );
